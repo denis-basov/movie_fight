600a0c1a5dec5179c1d50831d3dca032787a0c46
Denis Basov <den4er@mail.ru>
2022-12-12 22:28:38 +0300

    start implement search

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const fetchData = async () => {
+/**
+ * получение данных от API
+ */
+const fetchData = async (searchTerm) => {
 
     const response = await axios.get('http://www.omdbapi.com/', {
         params:{
            apikey: 'e74fe266',
-           s: 'blue'
+           s: searchTerm
         }
     });
     console.log(response.data);
 
-
 }
 
-fetchData();
+const input = document.querySelector('input');// получаем инпут
+
+let timeoutId;// идентификатор задержки
+// при вводе данных в поле ввода
+input.addEventListener('input', (event) => {
+    if(timeoutId){ // если есть id 
+        clearTimeout(timeoutId); // очищаем интервал
+    }
+    timeoutId = setTimeout(()=>{ // устанавливаем новый
+        fetchData(event.target.value);
+    }, 1000); // когда секунда пройдет, получаем данные
+    
+});
