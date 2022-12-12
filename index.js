be1457e10f0f40f7bccbb4dafe12120db3910d74
Denis Basov <den4er@mail.ru>
2022-12-12 18:28:34 +0300

    fetch movie

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hi there!');
+const fetchData = async () => {
+
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params:{
+           apikey: 'e74fe266',
+           s: 'blue'
+        }
+    });
+    console.log(response.data);
+
+
+}
+
+fetchData();
