ee2c57944d3f118ea034a1c37e25ab634b291319
Denis Basov <den4er@mail.ru>
2022-12-13 21:38:25 +0300

    render data about one movie

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ document.addEventListener('click', event => {
     }
 });
 
+// получаем данные об одном фильме
 const onMovieSelect = async movie => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params:{
@@ -79,4 +80,50 @@ const onMovieSelect = async movie => {
         }
     });
     console.log(response.data);
+    document.getElementById('summary').innerHTML = movieTemplate(response.data);
+};
+
+// формируем разметку
+const movieTemplate = movieDetail => {
+    return `
+        <article class="media">
+            <figure class="media-left">
+            <p class="image">
+                <img src="${movieDetail.Poster}" alt="">
+            </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <h1>${movieDetail.Title}</h1>
+                    <h4>${movieDetail.Genre}</h4>
+                    <p>${movieDetail.Plot}</p>
+                </div>
+            </div>
+        </article>
+
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Awards}</p>
+            <p class="subtitle">Awards</p>
+        </article>
+
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.BoxOffice}</p>
+            <p class="subtitle">Box Office</p>
+        </article>
+
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Metascore}</p>
+            <p class="subtitle">Metascore</p>
+        </article>
+
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.imdbRating}</p>
+            <p class="subtitle">IMDB Rating</p>
+        </article>
+
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.imdbVotes}</p>
+            <p class="subtitle">IMDB Votes</p>
+        </article>
+    `;
 };
