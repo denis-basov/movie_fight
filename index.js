3b5e0f631d44cf1d09aee4289fb870cc2fdb68f0
Denis Basov <den4er@mail.ru>
2022-12-13 13:22:12 +0300

    implement saerch dropdown

--- index.js
+++ index.js
@@ -9,20 +9,62 @@ const fetchData = async (searchTerm) => {
            s: searchTerm
         }
     });
-    console.log(response.data);
+
+    if(response.data.Error){
+        return [];
+    }
+    return response.data.Search;
 
 }
 
-const input = document.querySelector('input');// получаем инпут
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search for a movie</b></label>
+    <input class="input">
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
 
-let timeoutId;// идентификатор задержки
-// при вводе данных в поле ввода
-input.addEventListener('input', (event) => {
-    if(timeoutId){ // если есть id 
-        clearTimeout(timeoutId); // очищаем интервал
+const input = document.querySelector('input');// получаем инпут
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+  
+const onInput = async event => {
+    const movies = await fetchData(event.target.value); // получаем данные
+    if (!movies.length) {
+        dropdown.classList.remove('is-active');
+        return;
     }
-    timeoutId = setTimeout(()=>{ // устанавливаем новый
-        fetchData(event.target.value);
-    }, 1000); // когда секунда пройдет, получаем данные
+    resultsWrapper.innerHTML = ''; // удаляем контент предыдужего запроса
+    dropdown.classList.add('is-active'); // показываем выпадающее меню
     
+    // выводим найденные данные
+    movies.forEach(movie => {
+        const option = document.createElement('a');
+        option.classList.add('dropdown-item');
+
+        option.innerHTML = `
+            <img src="${movie.Poster === 'N/A' ? '' : movie.Poster}">
+            <h1>${movie.Title}</h1>
+        `;
+        
+        resultsWrapper.insertAdjacentElement('beforeend', option);
+
+        // если нажали на кино, скрываем выпадающее меню
+        option.addEventListener('click', event => {
+            dropdown.classList.remove('is-active');
+        });
+    });
+}
+
+input.addEventListener('input', debounce(onInput, 1000));
+
+// если кликнули в любое другое место на странице кроме элемента <div class="autocomplete">
+document.addEventListener('click', event => {
+    if (!root.contains(event.target)) {
+        dropdown.classList.remove('is-active');// закрываем выпадающий список
+    }
 });
